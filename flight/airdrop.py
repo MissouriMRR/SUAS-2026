3bd6fe59f35de9c91111b2c7e6400667382ab343
RexBerry <59031902+RexBerry@users.noreply.github.com>
2024-11-21 19:55:37 -0600

    Add parameters to docstring of set_servo()

--- flight/airdrop.py
+++ flight/airdrop.py
@@ -5,7 +5,18 @@ from pymavlink.mavutil import mavlink
 
 
 async def set_servo(vehicle: Vehicle, servo_num: int, pwm: int) -> None:
-    """Set the PWM of an auxillary pin on the drone."""
+    """
+    Set the PWM of an auxillary pin on the drone.
+
+    Parameters
+    ----------
+    vehicle : Vehicle
+        The drone containing the servo.
+    servo_num : int
+        The servo instance number.
+    pwm : int
+        The pulse width modulation, in microseconds.
+    """
     vehicle.message_factory.command_long_send(
         0, 0, mavlink.MAV_CMD_DO_SET_SERVO, 0, servo_num + 9, pwm, 0, 0, 0, 0, 0
     )
