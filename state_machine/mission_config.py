86cbcb3ce3f5bf94315bcb6c37290dd1e5e2803f
RexBerry <59031902+RexBerry@users.noreply.github.com>
2025-02-13 19:51:41 -0600

    Add docstrings to mission_config.py #5

--- state_machine/mission_config.py
+++ state_machine/mission_config.py
@@ -1,8 +1,31 @@
+"""Gets the mission configuration."""
+
 import json
 from typing import TextIO, TypedDict
 
 
 class MissionConfig(TypedDict):
+    """
+    A configuration for a flight mission.
+
+    Attributes
+    ----------
+    run_title : str
+        The name for the current flight operation
+    run_description : str
+        A small description for the current flight
+    sim_flag : bool
+        A flag representing if the connected drone is a simulation
+    mission_data_path : str
+        The path to the JSON file containing the boundary and waypoint data.
+    skip_waypoint : bool
+        Whether to skip the waypoint state.
+    skip_odlc_and_airdrop : bool
+        Whether to skip the ODLC and airdrop states.
+    standard_object_count : int
+        The number of standard objects to attempt to find.
+    """
+
     run_title: str
     run_description: str
     sim_flag: bool
@@ -13,6 +36,14 @@ class MissionConfig(TypedDict):
 
 
 def get_mission_config() -> MissionConfig:
+    """
+    Get the mission configuration from mission_config.json
+
+    Returns
+    -------
+    MissionConfig
+        The mission configuration.
+    """
     config_file: TextIO
     with open("mission_config.json", "r", encoding="utf-8") as config_file:
         return json.load(config_file)
