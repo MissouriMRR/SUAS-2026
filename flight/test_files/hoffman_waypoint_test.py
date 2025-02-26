83ba8353d4aea6261665ded1ce751b2920345de8
RexBerry <59031902+RexBerry@users.noreply.github.com>
2025-02-25 19:37:19 -0600

    Add logging to hoffman waypoint test #5

--- flight/test_files/hoffman_waypoint_test.py
+++ flight/test_files/hoffman_waypoint_test.py
@@ -83,6 +83,7 @@ async def run(flight_settings: FlightSettings) -> None:
 #  the Lats and Longs array and the drone has arrived at each of them
 if __name__ == "__main__":
     try:
+        logging.basicConfig(level=logging.INFO)
         loop = asyncio.get_event_loop()
         loop.run_until_complete(run(FlightSettings.from_mission_config()))
     except KeyboardInterrupt:
