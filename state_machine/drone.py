c1af0ea81f9c40cee4a22a819e0b6932e715b0ac
HarperReinhardt <harperreinhardt@gmail.com>
2025-04-01 18:42:59 -0500

    fixed sim addresses

--- state_machine/drone.py
+++ state_machine/drone.py
@@ -120,7 +120,7 @@ class Drone:
         self._vehicle = (
             dronekit.connect(self.address, wait_ready=True)
             if self.baud is None
-            else dronekit.connect(self.address, wait_ready=True, baud=self.baud, timeout=60)
+            else dronekit.connect(self.address, wait_ready=True, baud=self.baud)
         )
         logging.info("Drone discovered!")
 
@@ -215,13 +215,13 @@ class Drone:
         """
         match sim_mode:
             case SimMode.REAL:
-                self.address = "udpout:192.168.43.1:14552"
-                self.baud = None
+                self.address = "/dev/ttyFTDI"
+                self.baud = 921600
             case SimMode.SIM:
-                self.address = "tcp:127.0.0.1:5762"
+                self.address = "127.0.0.1:14030"
                 self.baud = None
             case SimMode.AIRSIM:
-                self.address = "127.0.0.1:14030"
+                self.address = "tcp:127.0.0.1:5762"
                 self.baud = None
             case _:
                 raise ValueError("invalid sim mode")
