87658e2794869b63f4c16deec595b7373d51549b
RexBerry <59031902+RexBerry@users.noreply.github.com>
2025-04-08 19:10:47 -0500

    Wait for user input after connecting in real mode

--- state_machine/drone.py
+++ state_machine/drone.py
@@ -27,6 +27,9 @@ class Drone:
     odlc_scan : bool
         A boolean to tell if the odlc zone needs to be scanned, used the
         first run and if odlc needs to be scanned any other time
+    sim_mode
+    _sim_mode : SimMode | None
+        The simulation mode of this drone, or None if one has not been specified.
     vehicle
     _vehicle : dronekit.Vehicle | None
         The Dronekit Vehicle object that controls the drone, or None if a connection
@@ -64,6 +67,7 @@ class Drone:
         baud : int, default None
             The baud rate, or None to use the default.
         """
+        self._sim_mode: SimMode | None = None
         self._vehicle: dronekit.Vehicle | None = None
         self.address: str = address
         self.baud: int | None = baud
@@ -82,6 +86,17 @@ class Drone:
             Whether this Drone object has connected to a drone.
         """
         return self._vehicle is not None
+
+    @property
+    def sim_mode(self) -> SimMode | None:
+        """Get the simulation mode of this drone.
+
+        Returns
+        -------
+        SimMode | None
+            The simulation mode of this drone, or None if one has not been specified.
+        """
+        return self._sim_mode
 
     @property
     def vehicle(self) -> dronekit.Vehicle:
@@ -123,6 +138,17 @@ class Drone:
             else dronekit.connect(self.address, wait_ready=True, baud=self.baud)
         )
         logging.info("Drone discovered!")
+
+        if self._sim_mode is not SimMode.REAL:
+            return
+
+        message_1: str = "Waiting for user input to continue... "
+        message_2: str = "(press enter when ready) "
+        input(
+            f"\x1b[38;2;255;255;0m{message_1}"
+            f"\x1b[3m{message_2}"
+            "\x1b[0m"
+        )
 
     def remove_arming_check(self) -> None:
         """
@@ -201,7 +227,8 @@ class Drone:
         self.vehicle.close()
 
     def use_settings(self, sim_mode: SimMode) -> None:
-        """Modify the connection settings based on the given simulation mode.
+        """Set the simulation mode of this drone and update the connection settings
+        according to the simulation mode.
 
         Parameters
         ----------
@@ -213,6 +240,8 @@ class Drone:
         ValueError
             If `sim_mode` is not a valid SimMode.
         """
+        self._sim_mode = sim_mode
+
         match sim_mode:
             case SimMode.REAL:
                 self.address = "/dev/ttyFTDI"
