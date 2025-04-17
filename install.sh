b9ce014ee1bc1783d717097d651482ea42a9b9fe
heitmeih <harry.heitmeier@gmail.com>
2025-04-17 10:42:23 -0500

    remove redudant install

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-sudo apt update && sudo apt install podman pipx
+sudo apt update && sudo apt install -y podman pipx
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-sudo apt-get update && sudo apt-get install -y pipx nvidia-container-toolkit
+sudo apt-get update && sudo apt-get install nvidia-container-toolkit
 
 pipx install podman-compose
 pipx ensurepath
