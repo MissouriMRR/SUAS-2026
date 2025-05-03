10713b72faf8d794b8318af774d9448803ead794
Ooglely <alennavdic@gmail.com>
2025-05-03 12:01:22 -0500

    Fix servo values

--- state_machine/drone.py
+++ state_machine/drone.py
@@ -266,7 +266,7 @@ class Drone:
         """
         if servo_num < 1 or servo_num > 4:
             raise ValueError("Servo number must be between 1 and 4")
-        open_values: list[int] = [2000, 1100, 1900, 1100]
+        open_values: list[int] = [950, 1600, 1950, 1900]
         await self._send_servo_msg(servo_num + 8, open_values[servo_num - 1])
 
     async def close_servo(self, servo_num: int) -> None:
@@ -282,7 +282,7 @@ class Drone:
         """
         if servo_num < 1 or servo_num > 4:
             raise ValueError("Servo number must be between 1 and 4")
-        closed_values: list[int] = [1100, 1600, 1100, 1900]
+        closed_values: list[int] = [2000, 1100, 1100, 1100]
         await self._send_servo_msg(servo_num + 8, closed_values[servo_num - 1])
 
     async def close(self) -> None:
