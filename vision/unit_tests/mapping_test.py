d513be25edce81f928fd00d7940b03155e7af2fe
Ooglely <alennavdic@gmail.com>
2026-06-13 14:36:38 -0500

    Add logging to mapping test, remove old unneeded test files

--- vision/unit_tests/mapping_test.py
+++ vision/unit_tests/mapping_test.py
@@ -1,17 +1,21 @@
-from vision.mapping_pipeline import mapping_pipeline
+"""Tests the mapping pipeline, using photos and data from the standard paths."""
+
 import asyncio
+import logging
+
+from vision.mapping_pipeline import mapping_pipeline
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
     capture_status = asyncio.Event()
     capture_status.set()  # Signal that all images are captured
 
-    print("AHH")
     asyncio.run(
         mapping_pipeline(
-            "./vision/mapping/datasets/camera.json",
-            "./vision/mapping/datasets/ODLC-Flight-2",
+            "flight/data/mapping_photos.json",
+            "mapping_images",
             capture_status,
-            "",
             "vision/mapping/results",
         )
     )
