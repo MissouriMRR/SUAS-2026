b7598dae6f56e808a7917f09e0ca92c4376af2db
Ooglely <alennavdic@gmail.com>
2026-09-08 10:22:31 -0500

    [#40] Fix comments. add --offline to proxy scripts

--- start_proxy_sim.sh
+++ start_proxy_sim.sh
@@ -5,4 +5,4 @@ source .venv/bin/activate
 # Start proxy
 # Primary and secondary should come from the sim or from additional mavproxy outputs
 # Output: udpout:127.0.0.1:14750, udpout:127.0.0.1:14751
-uv run -m proxy --primary udp:127.0.0.1:14650 --secondary udp:127.0.0.1:14651 --out udpout:127.0.0.1:14750 --out udpout:127.0.0.1:14751
+uv run --offline -m proxy --primary udp:127.0.0.1:14650 --secondary udp:127.0.0.1:14651 --out udpout:127.0.0.1:14750 --out udpout:127.0.0.1:14751
